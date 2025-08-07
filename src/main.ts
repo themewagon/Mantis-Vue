741c7d6ade635f503d4a1290b601b8c951c35fab
KhanAnupamShafi <deep71.bd@gmail.com>
2025-08-07 14:43:17 +0600

    init project

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { router } from './router';
+import vuetify from './plugins/vuetify';
+import '@/scss/style.scss';
+import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar';
+import VueTablerIcons from 'vue-tabler-icons';
+import VueApexCharts from 'vue3-apexcharts';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
+// google-fonts
+import '@fontsource/public-sans/400.css';
+import '@fontsource/public-sans/500.css';
+import '@fontsource/public-sans/600.css';
+import '@fontsource/public-sans/700.css';
+
+//Mock Api data
+import { fakeBackend } from '@/utils/helpers/fake-backend';
+
+const app = createApp(App);
+fakeBackend();
+app.use(router);
+app.use(PerfectScrollbarPlugin);
+app.use(createPinia());
+app.use(VueTablerIcons);
+app.use(Antd);
+app.use(VueApexCharts);
+app.use(vuetify).mount('#app');
