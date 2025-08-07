6cfcf307776451086212418ccf84f8ecf45e8e0f
KhanAnupamShafi <deep71.bd@gmail.com>
2025-08-07 16:25:28 +0600

    Updates

---
+++ assets/BlankLayout-Dc6cRU3v.js
@@ -0,0 +1,1 @@
+import{x as e,o,c as r,w as t,a,A as s,a1 as p,a2 as n}from"./index-BpJDZsnW.js";import{L as c}from"./LoaderWrapper-DY0mdz_z.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d=e({__name:"BlankLayout",setup(u){return(_,m)=>(o(),r(n,null,{default:t(()=>[a(c),a(s(p))]),_:1}))}});export{d as default};
